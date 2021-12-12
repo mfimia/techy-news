65caae2c9e0264d809e97b9bc81fa57a6de85f6d
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-12 09:41:24 +0100

    set up alert context

--- src/context/types.js
+++ src/context/types.js
@@ -1,3 +1,5 @@
 export const GET_NEWS = "GET_NEWS";
 export const SEARCH_NEWS = "SEARCH_NEWS";
 export const LOAD = "LOAD";
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
