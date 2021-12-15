3b473174d5774e2c7da066669a19cef1411f5a32
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-15 07:25:29 +0100

    storing user's mode preference in local storage

--- src/context/darkMode/DarkState.js
+++ src/context/darkMode/DarkState.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 import DarkContext from "./DarkContext";
 
 const DarkState = (props) => {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState(
+    JSON.parse(localStorage.getItem("darkModePref-techyNews") || "light")
+  );
+  localStorage.setItem("darkModePref-techyNews", JSON.stringify(mode));
 
   // Toggle dark mode
   const toggleMode = () => {
