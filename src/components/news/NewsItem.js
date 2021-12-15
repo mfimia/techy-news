82c30a3601328c4c45fe37895933bd573b5f4a3d
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-15 07:25:43 +0100

    styled url text and added tooltip

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -1,6 +1,8 @@
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { Link, Typography } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
 
 const NewsItem = ({ entry }) => {
   const { points, author, title, url, num_comments } = entry;
@@ -8,19 +10,36 @@ const NewsItem = ({ entry }) => {
     <ListItem>
       <ListItemText
         primary={
-          <Link underline="none" href={url}>
-            {title}
-          </Link>
+          <Tooltip
+            enterDelay={500}
+            TransitionComponent={Zoom}
+            title={url}
+            arrow
+          >
+            <Link underline="none" href={url}>
+              <Typography
+                component="span"
+                color={url ? "primary" : "secondary"}
+              >
+                {title}
+              </Typography>
+            </Link>
+          </Tooltip>
         }
         secondary={
           <Typography color="text.secondary" variant="body2">
-            <Typography component="span">{`${points} point${
-              points === 1 ? "" : "s"
-            } by `}</Typography>
-            <Typography component="span" sx={{ fontStyle: "italic" }}>
+            <Typography sx={{ fontSize: 14 }} component="span">
+              {`${points} point${points === 1 ? "" : "s"} by `}
+            </Typography>
+            <Typography
+              component="span"
+              sx={{ fontStyle: "italic", fontSize: 14 }}
+            >
               {author}
             </Typography>
-            <Typography component="span">{` | ${num_comments} comments`}</Typography>
+            <Typography sx={{ fontSize: 14 }} component="span">
+              {` | ${num_comments} comments`}
+            </Typography>
           </Typography>
         }
       ></ListItemText>
